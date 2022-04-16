d8e1a215a3925d8512c578e3095c8caa102a56ea
Abeer Naeem <abeernaeem6@gmail.com>
2022-04-16 16:38:33 -0500

    GET routes done

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs')
 const notes = require('./db/db.json');
+const { json } = require('express');
 const PORT = 3001;
 
 const app = express();
@@ -12,14 +13,29 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './public/index.html'));
+
+//API ROUTES
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+
+
+
+
+
+
+
+// HTML ROUTES
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });
